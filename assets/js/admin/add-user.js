a65adcd5a47edcc4678e2cd5b511c8fd43e339bd
Mauro Maia <dev@maurofilipemaia.dev>
2023-09-16 16:12:51 +0100

    add eslint

---
+++ assets/js/admin/add-user.js
@@ -0,0 +1,21 @@
+"use strict"
+
+$(document).ready(function () {
+	$("#firstName, #lastName, #email, #password, #password-conf").change(function () {
+
+		const firstName = $("#firstName").val()
+		const lastName = $("#lastName").val()
+		const email = $("#email").val()
+		const password = $("#password").val()
+		const password_conf = $("#password-conf").val()
+
+		$("#save").disabled = !(
+			password.length >= 8
+			&& password_conf.length >= 8
+			&& password_conf === password
+			&& firstName.length > 0
+			&& lastName.length > 0
+			&& email.length > 0
+		)
+	})
+})
